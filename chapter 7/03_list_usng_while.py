50459acd0ae133967fcf3967b9fdf19b5e2113c5
Abhay895806 <abhay170705@gmail.com>
2026-08-11 11:48:15 +0530

    practice loop

---
+++ chapter 7/03_list_usng_while.py
@@ -0,0 +1,5 @@
+l = [1,"Harry",False,"this","Rohan","Shubham"]
+i =0
+while(i<len(l)):
+    print(l[i])
+    i+=1
---
+++ chapter 7/05_for_loop_iterate.py
@@ -0,0 +1,16 @@
+#for loop with lists
+# l =[1,4,2,3,6]
+# for i in l:
+#     print(l)
+
+
+
+#for loop with tuples
+# t = (6,45,321,678)
+# for i in t:
+#     print(i)
+
+#for loop with strings
+s = "Abhay"
+for i in s:
+    print(i)
