3dbbbc62e9499b1ae63fbe809ea5b79781c6c33c
Abhay895806 <abhay170705@gmail.com>
2026-08-13 20:23:15 +0530

    functionand recursion practice

---
+++ chapter 8/01_function.py
@@ -0,0 +1,29 @@
+# a = int(input("Enter your number:"))
+# b = int(input("Enter your number:"))
+# c = int(input("Enter your number:"))
+
+# average = (a+b+c)/3
+# print(average)
+# a = int(input("Enter your number:"))
+# b = int(input("Enter your number:"))
+# c = int(input("Enter your number:"))
+
+# average = (a+b+c)/3
+# print(average)
+
+
+# function defination
+def avg():
+    a = int(input("Enter your number:"))
+    b = int(input("Enter your number:"))
+    c = int(input("Enter your number:"))
+
+    average = (a+b+c)/3
+    print(average)
+
+    # function call
+avg()
+print("Thank You")
+avg()
+print("Thank You")
+avg()
